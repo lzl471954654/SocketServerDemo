c54da89ec080223a1803bb4871acf6ed9d010f18
LZL <471954654@qq.com>
2017-09-05 18:22:43 +0800

    split controlledServerThread and controlServerThread

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,4 +1,5 @@
 import JavaBean.Account;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -9,7 +10,7 @@ public class ServerMain {
     public static ConcurrentHashMap<Account,ServerThread> controlSocketMap = new ConcurrentHashMap<>();
     public static ConcurrentHashMap<Account,ControlledServerThread> beControlledSocketMap = new ConcurrentHashMap<>();
 
-    public void serverRun() {
+    /*public void serverRun() {
         try {
             ServerSocket serverSocket = new ServerSocket(10086);
             while(true){
@@ -23,5 +24,61 @@ public class ServerMain {
             LogUtils.logException(this.getClass().getName(),"请检查端口是否被绑定\t"+e.getMessage());
             LogUtils.releaseResource();
         }
+    }*/
+
+    public void serverRun(){
+        try{
+            run();
+        }catch (Exception e){
+            e.printStackTrace();
+            LogUtils.logException(getClass().getName(),""+e.getMessage());
+            LogUtils.releaseResource();
+        }
+    }
+
+    private void run(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ServerSocket serverSocket;
+                try{
+                    serverSocket = new ServerSocket(10087);
+                }catch (IOException e){
+                    e.printStackTrace();
+                    LogUtils.logException(this.getClass().getName(),"请检查端口是否被绑定\t"+e.getMessage());
+                    LogUtils.releaseResource();
+                    return;
+                }
+                try{
+                    while(true){
+                        Socket socket = serverSocket.accept();
+                        ControlledServerThread thread = new ControlledServerThread(socket);
+                        LogUtils.logInfo(getClass().getName(),"Get a ControlledSocket -> IP: "+socket.getInetAddress()+" Port: "+socket.getPort());
+                        thread.start();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                    LogUtils.logException(this.getClass().getName(),"\t"+e.getMessage());
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    ServerSocket serverSocket = new ServerSocket(10086);
+                    while(true){
+                        Socket socket = serverSocket.accept();
+                        ServerThread thread = new ServerThread(socket);
+                        LogUtils.logInfo(getClass().getName(),"Get a ControlledSocket -> IP: "+socket.getInetAddress()+" Port: "+socket.getPort());
+                        thread.start();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                    LogUtils.logException(this.getClass().getName(),"请检查端口是否被绑定\t"+e.getMessage());
+                }
+            }
+        }).start();
     }
 }
